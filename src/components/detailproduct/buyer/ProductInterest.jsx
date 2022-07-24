dc2e256401a55f5cfd05d689341b749ead89679b
akbarrmn <akbar.rahmana@gmail.com>
2022-07-24 19:20:28 +0700

    Push new logic wishlist

--- src/components/detailproduct/buyer/ProductInterest.jsx
+++ src/components/detailproduct/buyer/ProductInterest.jsx
@@ -1,12 +1,12 @@
 import { Box, Button, IconButton, Typography } from '@mui/material'
-import React , { useState }from 'react'
+import React, { useState } from 'react'
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { deleteWishlist, fetchWishlist, postWishlist } from '../../../redux/wishlist';
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom';
 import { fetchTawar } from '../../../redux/tawar';
 
-const ProductInterest = ({data,handleOpen}) => {
+const ProductInterest = ({ data, handleOpen }) => {
     // Wishlist
     const [love, setLove] = useState(false)
     const formatter = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" })
@@ -24,15 +24,13 @@ const ProductInterest = ({data,handleOpen}) => {
 
     const handleWishlist = async () => {
         if (Object.keys(dataUser).length !== 0 && dataUser.id !== detailProduct.user.id) {
-            if (love === false) {
+            if (love === false && Object.keys(detailWishlist).length === 0) {
                 setLove(true)
-                const data =  {
-                    product_id : id
+                const data = {
+                    product_id: id
                 }
-                if (Object.keys(detailWishlist).length === 0) {
-                    dispatch(postWishlist(data)).then(res => res.payload.success && dispatch(fetchWishlist()))
-                }
-            }else{
+                dispatch(postWishlist(data)).then(res => res.payload.success && dispatch(fetchWishlist()))
+            } else {
                 setLove(false)
                 const detailWishlist = wishlistAmbil.filter(item => item.productId === Number(id))
                 const dataId = detailWishlist[0].id
@@ -44,43 +42,43 @@ const ProductInterest = ({data,handleOpen}) => {
     }
 
     // Tawar
-    const dataTawar = useSelector(state=>state.tawar.tawar)
+    const dataTawar = useSelector(state => state.tawar.tawar)
     const tawarID = Object.keys(dataTawar).length !== 0 ? dataTawar.filter(item => item.productId === Number(id) && item.userId === dataUser.id) : ''
     console.log(dataTawar)
 
     React.useEffect(() => {
         dispatch(fetchTawar())
-    },[dispatch])
+    }, [dispatch])
 
-    React.useEffect(()=>{
+    React.useEffect(() => {
         if (onWishlist) {
             setLove(true)
-        }else{
+        } else {
             setLove(false)
         }
         dispatch(fetchWishlist())
-    },[dispatch,onWishlist])
+    }, [dispatch, onWishlist])
 
     return (
         <>
             <Box component={'div'} rowGap={2} p={4} flexDirection={'column'} display={'flex'} sx={{ boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.15)', borderRadius: '16px' }}>
                 <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
-                    <Typography variant='h6' fontWeight={800} sx={{ maxWidth:'400px',textOverflow:'ellipsis', whiteSpace:'nowrap', overflow:'hidden' }}>
-                        {data? data.name : ''}
+                    <Typography variant='h6' fontWeight={800} sx={{ maxWidth: '400px', textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' }}>
+                        {data ? data.name : ''}
                     </Typography>
                     <IconButton onClick={handleWishlist} sx={{ padding: .5 }} >
-                        <FavoriteIcon sx={{ fontSize:'2rem', color: love? 'red':'unset'  }}/>
+                        <FavoriteIcon sx={{ fontSize: '2rem', color: love ? 'red' : 'unset' }} />
                     </IconButton>
                 </Box>
                 <Typography variant='h6' sx={{ color: '#8A8A8A' }}>
-                    {data? data.category : ''}
+                    {data ? data.category : ''}
                 </Typography>
-                <Typography variant='h6' sx={{ maxWidth:'200px',textOverflow:'ellipsis', whiteSpace:'nowrap', overflow:'hidden' }}>
-                     {data? formatter.format(data.price) : ''}
+                <Typography variant='h6' sx={{ maxWidth: '200px', textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' }}>
+                    {data ? formatter.format(data.price) : ''}
                 </Typography>
-                
-                <Button color='primary' variant='contained' disabled={tawarID.length !== 0 || data.isSold} onClick={handleOpen} sx={{ borderRadius: '16px', height: 'auto', minHeight:'48px', display: { md: 'block', xs: 'none' } }}>
-                    {Object.keys(tawarID).length === 0 ?  'Saya tertarik ingin nego' : data.isSold ? 'Produk telah terjual' : 'Menunggu respon penjual'}
+
+                <Button color='primary' variant='contained' disabled={tawarID.length !== 0 || data.isSold} onClick={handleOpen} sx={{ borderRadius: '16px', height: 'auto', minHeight: '48px', display: { md: 'block', xs: 'none' } }}>
+                    {Object.keys(tawarID).length === 0 ? 'Saya tertarik ingin nego' : data.isSold ? 'Produk telah terjual' : 'Menunggu respon penjual'}
                 </Button>
             </Box>
         </>
